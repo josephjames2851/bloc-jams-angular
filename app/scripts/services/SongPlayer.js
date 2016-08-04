f6dcfb722adcaafc0d15326d6be715b61766d2fb
Joe Weinzettel <josephjames2851@gmail.com>
2016-08-04 17:01:00 -0500

    assignment 7 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
     function SongPlayer() {
         var SongPlayer = {};
         
+         /**
+        * @desc Song that is currently playing
+        * @type {Object}
+        */
         var currentSong = null;
         
         /**
@@ -15,8 +19,6 @@
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
         * @param {Object} song
         */
-        var currentBuzzObject = null;
-        
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
@@ -31,11 +33,25 @@
             currentSong = song;
         };
         
+        /**
+        * @function playSong
+        * @desc Plays the currentBuzzObject and set the song's playing property to true
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
+        /**
+        * @function play
+        * @desc Plays a new song or pauses the currently playing song
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
@@ -43,6 +59,11 @@
             }
         };
         
+        /**
+        * @function pause
+        * @desc Pauses the currentBuzzObject and sets the song playing property to false
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
