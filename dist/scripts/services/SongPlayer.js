42aca62d5c586bacfcc61a23feebbd0c99140a11
Joe Weinzettel <josephjames2851@gmail.com>
2016-08-10 13:51:23 -0500

    checkpoint 10 bugs resolved on master branch

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -32,7 +32,7 @@
             
             currentBuzzObject.bind('timeupdate', function() {
                 $rootScope.$apply(function() {
-                    songPlayer.currentTime = currentBuzzObject.getTime();
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
                 });
             });
             
